4ebbf15d5338139dac0212283dd9c95ef29c0c9a
Oğuzhan SANCAR <130090457+sancarhan@users.noreply.github.com>
2026-04-02 19:34:24 +0300

    .

--- Sosyal Medya/client/src/assets/assets.js
+++ Sosyal Medya/client/src/assets/assets.js
@@ -28,8 +28,8 @@ export const menuItemsData = [
 export const dummyUserData = {
     "_id": "user_2zdFoZib5lNr614LgkONdD8WG32",
     "email": "admin@example.com",
-    "full_name": "John Warren",
-    "username": "john_warren",
+    "full_name": "Oğuzhan SANCAR",
+    "username": "sancarhanyazilim",
     "bio": "🌍 Dreamer | 📚 Learner | 🚀 Doer\r\nExploring life one step at a time.\r\n✨ Staying curious. Creating with purpose.",
     "profile_picture": sample_profile,
     "cover_photo": sample_cover,
