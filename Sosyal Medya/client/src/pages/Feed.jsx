0628e80d5547b2ba00d5c9bec33a1edd646444a4
Oğuzhan SANCAR <130090457+sancarhan@users.noreply.github.com>
2026-04-03 19:21:03 +0300

    .

--- Sosyal Medya/client/src/pages/Feed.jsx
+++ Sosyal Medya/client/src/pages/Feed.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { dummyPostsData } from '../assets/assets'
+import Loading from '../components/Loading'
 
 const Feed = () => {
-  return (
-    <div>
-      
+
+  const [feeds, setfeeds] = useState([])
+  const [loading, setLoading] = useState(true)
+
+
+  const fetchFeeds = async () => {
+    setfeeds(dummyPostsData)
+    setLoading(false)
+
+  }
+
+  useEffect(()=>{
+    fetchFeeds()
+  },[])
+
+  return !loading ? (
+    <div className='h-full overflow-y-scroll no-scrollbar py-10 xl:pr-5 flex items-start justify-center xl:gap-8'>
+      {/* hikaye ve gönderi listesi */}
+      <div>
+        <h1>Buradaki hikayeler</h1>
+        <div className='p-4 space-y-6'>
+          Gönderi listesi
+        </div>
+      </div>
+
+      {/* sağ sidebar */}
+      <div>
+        <div>
+          Sponsorlu
+        </div>
+        <h1>Son mesajlar</h1>
+      </div>
     </div>
-  )
+  ) : <Loading />
 }
 
 export default Feed
