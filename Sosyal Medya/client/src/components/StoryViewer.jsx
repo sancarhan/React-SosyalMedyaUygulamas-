110bd476be862ce04e1741d883c63d48116bc179
Oğuzhan SANCAR <130090457+sancarhan@users.noreply.github.com>
2026-04-12 23:31:07 +0300

    .

--- Sosyal Medya/client/src/components/StoryViewer.jsx
+++ Sosyal Medya/client/src/components/StoryViewer.jsx
@@ -66,6 +66,8 @@ const StoryViewer = ({viewStory, setViewStory}) => {
     }
   }
 
+  const
+
   return (
     <div className='fixed inset-0 h-screen bg-black bg-opacity-90 z-110 flex items-center justify-center'
     style={{backgroundColor: viewStory.media_type === 'text' ? viewStory.background_color : '#000000'}}>
