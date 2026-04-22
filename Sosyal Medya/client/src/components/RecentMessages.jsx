8530eee2d94c96b7d7e5a1016264729a4ee56752
Oğuzhan SANCAR <130090457+sancarhan@users.noreply.github.com>
2026-04-22 20:13:36 +0300

    .

--- Sosyal Medya/client/src/components/RecentMessages.jsx
+++ Sosyal Medya/client/src/components/RecentMessages.jsx
@@ -1,32 +1,43 @@
-import React, { useEffect, useState } from 'react'
-import { dummyRecentMessagesData } from '../assets/assets'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { dummyRecentMessagesData } from "../assets/assets";
+import { Link } from "react-router-dom";
+import moment from "moment";
 
 const RecentMessages = () => {
+  const [messages, setMessages] = useState([]);
 
- const [messages, setMessages] = useState([])
-
- const fetchRecentMessages = async () => {
-  setMessages(dummyRecentMessagesData)
- }
- useEffect (()=>{
-  fetchRecentMessages();
- },[])
+  const fetchRecentMessages = async () => {
+    setMessages(dummyRecentMessagesData);
+  };
+  useEffect(() => {
+    fetchRecentMessages();
+  }, []);
 
   return (
-    <div className='bg-white max-w-xs mt-4 p-4 min-h-20 rounded-md shadow text-xs text-slate-800'>
-      <h3 className='font-semibold text-slate-8 mb-4'>Son mesajlar</h3>
-      <div className='flex flex-col max-h-56 overflow-y-scroll no-scrollbar'>
-       {
-        messages.map((message, index)=>(
-         <Link key={index} className='flex items-start gap-2 py-2 hover:bg-slate-100'>
-          <img src={message.from_user_id.profile_picture} className='w-8 h-8 rounded-full' alt="" />
-         </Link>
-        ))
-       }
+    <div className="bg-white max-w-xs mt-4 p-4 min-h-20 rounded-md shadow text-xs text-slate-800">
+      <h3 className="font-semibold text-slate-8 mb-4">Son mesajlar</h3>
+      <div className="flex flex-col max-h-56 overflow-y-scroll no-scrollbar">
+        {messages.map((message, index) => (
+          <Link
+            key={index}
+            className="flex items-start gap-2 py-2 hover:bg-slate-100"
+          >
+            <img
+              src={message.from_user_id.profile_picture}
+              className="w-8 h-8 rounded-full"
+              alt=""
+            />
+            <div>
+              <div>
+                <p>{message.from_user_id.full_name}</p>
+                <p>{moment(message.createdAt).fromNow()}</p>
+              </div>
+            </div>
+          </Link>
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default RecentMessages
+export default RecentMessages;
