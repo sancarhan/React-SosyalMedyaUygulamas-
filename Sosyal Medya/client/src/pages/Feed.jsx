4872b476ad0dcb942907989a5556228ca93a2a3d
Oğuzhan SANCAR <130090457+sancarhan@users.noreply.github.com>
2026-04-22 19:57:46 +0300

    .

--- Sosyal Medya/client/src/pages/Feed.jsx
+++ Sosyal Medya/client/src/pages/Feed.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { dummyPostsData } from '../assets/assets'
+import { assets, dummyPostsData } from '../assets/assets'
 import Loading from '../components/Loading'
 import StoriesBar from '../components/StoriesBar'
 import PostCard from '../components/PostCard'
@@ -33,9 +33,11 @@ const Feed = () => {
       </div>
 
       {/* sağ sidebar */}
-      <div>
-        <div>
-          Sponsorlu
+      <div className='max-xl:hidden sticky top-0'>
+        <div className='max-w-xs bg-white text-xs p-4 rounded-md inline-flex flex-col gap-2 shadow'>
+          <h3 className='text-slate-800 font-semibold'>Sponsorlu</h3>
+          <img src={assets.sponsored_img} className='w-75 h-50 rounded-md' alt="" />
+          <p></p>
         </div>
         <h1>Son mesajlar</h1>
       </div>
