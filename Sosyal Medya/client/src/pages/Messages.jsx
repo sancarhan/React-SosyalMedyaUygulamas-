acd9067e4e071d26ca0623a52263d233da07229e
Oğuzhan SANCAR <130090457+sancarhan@users.noreply.github.com>
2026-04-24 17:25:48 +0300

    .

--- Sosyal Medya/client/src/pages/Messages.jsx
+++ Sosyal Medya/client/src/pages/Messages.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 const Messages = () => {
   return (
-    <div>
-      
+    <div className='min-h-screen relative bg-slate-50'>
+      <div className='max-w-6xl mx-auto p-6'>
+        {/* Başlık */}
+        <div>
+          <h1>Mesajlar</h1>
+          <p></p>
+        </div>
+      </div>
     </div>
   )
 }
