3956a9261258496eb451e13d216db26ccc3ac953
Oğuzhan SANCAR <130090457+sancarhan@users.noreply.github.com>
2026-04-24 17:27:40 +0300

    .

--- Sosyal Medya/client/src/pages/Messages.jsx
+++ Sosyal Medya/client/src/pages/Messages.jsx
@@ -5,10 +5,12 @@ const Messages = () => {
     <div className='min-h-screen relative bg-slate-50'>
       <div className='max-w-6xl mx-auto p-6'>
         {/* Başlık */}
-        <div>
-          <h1>Mesajlar</h1>
-          <p></p>
+        <div className='mb-8'>
+          <h1 className='text-3xl font-bold text-slate-900 mb-2'>Mesajlar</h1>
+          <p className='text-slate-600'>Arkadaşlarınızla ve ailenizle konuşun.</p>
         </div>
+
+        {/* Bağlı Kullanıcılar */}
       </div>
     </div>
   )
