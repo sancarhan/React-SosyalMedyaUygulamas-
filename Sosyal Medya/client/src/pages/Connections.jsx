4bc320a9838b7c2da617b8273801240d64b6efff
Oğuzhan SANCAR <130090457+sancarhan@users.noreply.github.com>
2026-04-27 19:25:43 +0300

    .

--- Sosyal Medya/client/src/pages/Connections.jsx
+++ Sosyal Medya/client/src/pages/Connections.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { Users, UserPlus, UserCheck, UserRoundPen, MessageSquare } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+import { dummyConnectionsData as connections,
+  dummyFollowersData as followers,
+  dummyFollowingData as following,
+  dummyPendingConnectionsData as pendingConnections
+ } from "../assets/assets";
 
 const Connections = () => {
+
+  const navigate = useNavigate()
+
+  const dataArray = [
+    {label: 'Takipçiler', value: followers, icon: Users},
+    {label: 'Takip ettiklerin', value: following, icon: UserCheck},
+    {label: 'Bekleyen İstekler', value: pendingConnections, icon: UserRoundPen},
+    {label: 'Bağlantılar', value: connections, icon: UserPlus},
+  ]
+
   return (
-    <div>
+    <div className='min-h-screen bg-slate-50'>
       
     </div>
   )
