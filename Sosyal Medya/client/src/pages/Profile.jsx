ac03fa4c9821acca47a2bdefa0a21bfe91408047
Oğuzhan SANCAR <130090457+sancarhan@users.noreply.github.com>
2026-05-06 19:57:07 +0300

    .

--- Sosyal Medya/client/src/pages/Profile.jsx
+++ Sosyal Medya/client/src/pages/Profile.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useParams } from "react-router-dom";
 
 const Profile = () => {
+
+  const {profileId} = useParams()
+  const [user, setUser] = useState(null)
+  const [posts, setPosts] = useState([])
+  const [activeTab, setActiveTab] = useState('posts')
+  const [showEdit, setShowEdit] = useState('false')
+
   return (
     <div>
       
