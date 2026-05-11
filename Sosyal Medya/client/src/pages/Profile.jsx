839599db8863d7ee1e86dbb679278aab536fc7c4
Oğuzhan SANCAR <130090457+sancarhan@users.noreply.github.com>
2026-05-11 20:20:48 +0300

    Update Profile.jsx
    .

--- Sosyal Medya/client/src/pages/Profile.jsx
+++ Sosyal Medya/client/src/pages/Profile.jsx
@@ -79,7 +79,7 @@ const Profile = () => {
       </div>
       {/* Profil düzenlemesi */}
       {showEdit && <p>Profil düzenlemesini göster</p> }
-      <div></div>
+      
     </div>
    
   ) : (
