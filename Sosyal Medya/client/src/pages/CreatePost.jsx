c0642e41b2b8a3727b35c0ca354e38bdf0172bf0
Oğuzhan SANCAR <130090457+sancarhan@users.noreply.github.com>
2026-05-12 18:21:29 +0300

    Update CreatePost.jsx
    .

--- Sosyal Medya/client/src/pages/CreatePost.jsx
+++ Sosyal Medya/client/src/pages/CreatePost.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { dummyUserData } from '../assets/assets'
 import { Image, X } from 'lucide-react'
+import toast from "react-hot-toast";
 
 const CreatePost = () => {
 
@@ -9,6 +10,10 @@ const CreatePost = () => {
   const [loading, setLoading] = useState(false)
   
   const user = dummyUserData;
+
+  const handleSubmit = async () => {
+    
+  }
 
   return (
     <div className='min-h-screen bg-gradient-to-b from-slate-50 to-white'>
@@ -61,7 +66,14 @@ const CreatePost = () => {
             <input type="file" id="images" accept='image/*' hidden multiple
              onChange={(e)=>setImages([...images, ...e.target.files])}/>
 
-             <button className='text-sm bg-gradient-to-r from-blue-500
+             <button disabled={loading} onClick={()=> toast.promise(
+              handleSubmit(),
+              {
+                loading: 'Yükleniyor...',
+                success: <p>Gönderi Yayınlandı</p>,
+                error: <p>Gönderi Yayınlanırken Hata Oluştu</p>,
+              }
+             )} className='text-sm bg-gradient-to-r from-blue-500
              to-purple-600 hover:from-blue-600 hover:to-purple-700
              active:scale-95 transition text-white font-medium px-8 py-2
              rounded-md cursor-pointer'>
